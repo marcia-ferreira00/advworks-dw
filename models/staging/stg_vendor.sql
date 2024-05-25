6046026caeb38e995de76a8e7ee41e87f71d47b2
monica.1993.gomes@gmail.com <monica.1993.gomes@gmail.com>
2024-05-25 16:09:35 +0000

    Alteramos o  stg_endor.sql e já corre. existe um erro que vou resolve nos snapshots.

--- models/staging/stg_vendor.sql
+++ models/staging/stg_vendor.sql
@@ -1,42 +1,35 @@
-with product as (
-    select 
-        productid as product_id,
-        name as product_name,
-        productnumber as product_number,
-        color as color,
-        size as size,
-        weight as weight,
-    from 
-        {{ source('production', 'product') }}
-),
+with
+    product as (
+        select
+            productid as product_id,
+            name as product_name,
+            productnumber as product_number,
+            productsubcategoryid as product_subcategoryid,
+            color as color,
+            size as size,
+            weight as weight
+        from {{ source('production', 'product') }}
+    ),
 
-subcategory as (
-    select 
-        productsubcategoryid,
-        name as subcategory,
-        productcategoryid
-    from 
-        {{ source('production', 'productsubcategory') }}
-),
+    subcategory as (
+        select productsubcategoryid, name as subcategory_name, productcategoryid
+        from {{ source('production', 'productsubcategory') }}
+    ),
 
-category as (
-    select 
-        productcategoryid,
-        name as category
-    from 
-        {{ source('production', 'productcategory') }}
-)
+    category as (
+        select productcategoryid, name as category_name
+        from {{ source('production', 'productcategory') }}
+    )
 
-select 
+select
     p.product_id,
     p.product_name,
     p.product_number,
-    c.category,
-    s.subcategory,
-    p.color,
+    c.category_name,
+    s.subcategory_name,
     p.size,
+    p.color,
     p.weight
-from 
-    product p
-    left join subcategory s on p.productsubcategoryid = s.productsubcategoryid
-    left join category c on s.productcategoryid = c.productcategoryid
+from product p
+left join subcategory s on p.product_subcategoryid = s.productsubcategoryid
+left join category c on s.productcategoryid = c.productcategoryid
