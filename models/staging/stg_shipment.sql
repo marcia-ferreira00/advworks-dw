83b96352cf085cf067d3667e9514d511d46feefd
Carolina Moura <moura.s.anacarolina@gmail.com>
2024-05-25 11:33:46 +0000

    commit

---
+++ models/staging/stg_shipment.sql
@@ -0,0 +1,8 @@
+select 
+    shipmethodid as shipment_id,
+    name,
+    shipbase as min_ship_charge,
+    shiprate as pound_ship_charge,
+    modifieddate as modified_date
+from 
+    {{ source('purchasing', 'shipmethod') }}
