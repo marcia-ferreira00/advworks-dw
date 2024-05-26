a7f4432878333a3ec5daf1d1f551039dd3ab5f2c
monica.1993.gomes@gmail.com <monica.1993.gomes@gmail.com>
2024-05-26 08:45:16 +0000

    Erros por resolver, sorry...  logo se precisares de ajuda me diz ;D

--- models/Final/dim_shipment.sql
+++ models/Final/dim_shipment.sql
@@ -1,25 +1,13 @@
-with first_version as (
-    select dbt_scd_id,
-            ship_method_id,
-            ship_name,
-            ship_base,
-            ship_rate,
-            dbt_valid_from,
-            dbt_valid_to,
-            dbt_updated_at,
-            row_number() over(partition by ship_method_id order by dbt_valid_from) as row_nr
+    select 
+            dbt_scd_id as ship_method_id
+            shipment_id,
+            timestamp,
+            purchasing,
     from {{ ref('sp_shipment') }}
-)
-select
-    dbt_scd_id as sk_shipment,
-    ship_method_id,
-    ship_name,
-    ship_base,
-    ship_rate,
-    case
-        when row_nr = 1 then '1970-01-01'
-        else dbt_valid_from
-    end as valid_from,
-    coalesce(dbt_valid_to, '2200-01-01') as valid_to,
-    dbt_updated_at as last_updated_at
-from first_version
+
+       config(
+        target_schema='snapshots',
+        unique_key='ship_method_id',
+        strategy='timestamp',
+        updated_at='modified_date'
+    )
