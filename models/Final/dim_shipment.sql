70085d0d2ef230c31e02ef5445182e61507c1f2c
Carolina Moura <moura.s.anacarolina@gmail.com>
2024-05-26 21:28:03 +0000

    Final

--- models/Final/dim_shipment.sql
+++ models/Final/dim_shipment.sql
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-    with first_version as (
+with first_version as (
     select  ship_method_id,
             ship_name,
             ship_base,
@@ -12,21 +11,3 @@ select
     ship_base,
     ship_rate
 from first_version
-=======
-
-{{
-    config(
-        target_schema='snapshots',
-        unique_key='ship_method_id',
-        strategy='timestamp',
-        updated_at='modified_date'
-    )
-}}    
-
-select 
-    dbt_scd_id as ship_method_id,
-    shipment_id,
-    timestamp,
-    purchasing
-from {{ ref('sp_shipment') }}
->>>>>>> 531ca9f52072891edee6dbff3b21ad5aa0f3ddb2
