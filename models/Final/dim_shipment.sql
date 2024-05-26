6856f3160d87bfdc3af02609f7e9884145057c88
monica.1993.gomes@gmail.com <monica.1993.gomes@gmail.com>
2024-05-26 18:51:15 +0000

    schema.yml snas fixed

--- models/Final/dim_shipment.sql
+++ models/Final/dim_shipment.sql
@@ -1,13 +1,16 @@
-    select 
-            dbt_scd_id as ship_method_id
-            shipment_id,
-            timestamp,
-            purchasing,
-    from {{ ref('sp_shipment') }}
 
-       config(
+{{
+    config(
         target_schema='snapshots',
         unique_key='ship_method_id',
         strategy='timestamp',
         updated_at='modified_date'
     )
+}}    
+
+select 
+    dbt_scd_id as ship_method_id,
+    shipment_id,
+    timestamp,
+    purchasing
+from {{ ref('sp_shipment') }}
